264f864802961f122063c0b6d2db252fcdafbf36
Alicja Namieta <anamieta@3-C-7.42heilbronn.de>
2023-11-30 15:08:09 +0100

    with comments

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,16 @@
 /*   By: anamieta <anamieta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 17:38:16 by anamieta          #+#    #+#             */
-/*   Updated: 2023/11/18 16:43:34 by anamieta         ###   ########.fr       */
+/*   Updated: 2023/11/30 15:07:42 by anamieta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
 char	*read_and_concatenate(int *bytes_read, int fd, char *line)
-// what is the line
+// what is the line - it is an empty string that waits for the content of the buffer
+// this function is focused on building the line as data is read, "/n" determines when to stop reading
+// doesn't conatin "\n" at the end of the line
 {
 	char	*buffer;
 	char	*concatenated;
@@ -30,10 +32,26 @@ char	*read_and_concatenate(int *bytes_read, int fd, char *line)
 			return (free(buffer), NULL);
 		buffer[*bytes_read] = '\0';
 // its failing tests if I get rid of this line, but why do we need it if we used calloc?
+// adding the null terminator explicitly can be seen as a defensive programming practice,
+// ensuring that the buffer remains null-terminated regardless of changes in the code or potential variations
+// in the memory allocation function used. It might be considered as a precautionary measure for code robustness.
 		concatenated = ft_strjoin(line, buffer);
 		free (line);
+		// In practice, freeing the memory of the initial empty string before reassigning it with 
+		// line = concatenated; is more of a defensive programming practice. It ensures that any memory 
+		// previously allocated for line is properly released before assigning it a new value. This helps 
+		// prevent memory leaks and ensures that the program does not consume more and more memory as it 
+		// iterates through reading lines from the file.
+
+		// Otherwise, you would lose reference to this memory block, and it would not be deallocated.
+		// So, the free(line) before reassignment is crucial for preventing memory leaks. It ensures that 
+		// the memory associated with the old content of line is properly released before assigning it to 
+		// a new value. This is a good practice for memory management to avoid unnecessary memory consumption 
+		// over multiple iterations.
 		line = concatenated;
-		if (ft_strchr(buffer, '\n'))
+		if (ft_strchr(buffer, '\n')) // checking for first occurrence of the newline char in buffer
+		// Placing the condition at the end of the loop ensures that the program checks for the presence of
+		// a newline character only after reading a chunk of data into the buffer.
 			break ;
 	}
 	free (buffer);
@@ -50,7 +68,16 @@ int	get_line_len(char *line)
 	return (i);
 }
 
+// read_and_concatenate actually created the line from a file, and its allocated and 
+// properly terminated by get_result
+
 char	*get_result(char *line, int bytes_read)
+// bytes_read is the number of characters obtained by get_line_len
+// The function allocates memory for the result based on the length of the line
+// contains "\n" before "\0"
+// it is used after read_and_concatenate has completed the process of building the line
+// specifically handles creating a result string from the content of line, including the newline character 
+// if it exists.
 {
 	char	*result;
 	int		i;
@@ -62,6 +89,7 @@ char	*get_result(char *line, int bytes_read)
 	if (!result)
 		return (NULL);
 	while (line[i] != '\n' && line[i])
+	// while it's not a newline character or null terminator
 	{
 		result[i] = line[i];
 		i++;
@@ -82,7 +110,10 @@ char	*allocate_new_line(char *line, int bytes_read)
 		free(line);
 		return (NULL);
 	}
+	// the purpose is to free it always because we dont need it
 	new_line = ft_calloc((ft_strlen(line) - bytes_read + 1), sizeof(char));
+	// after freeing the line, it is still used here up in strlen and below, why is it working?
+	// at the bytes_read theres always a null terminator, so we're basically always freeing it
 	if (!new_line)
 		return (NULL);
 	bytes_read++;
@@ -144,4 +175,25 @@ char	*get_next_line(int fd)
 //     // Clean up
 //     close(file);
 //     return 0;
+// }
+
+// void init_gnl(t_gnl *gnl, int argc, char **argv, char **env)
+// {
+// 	gnl->argc = argc;
+// 	gnl->argv = argv;
+// 	gnl->env = env;
+// 	gnl->word = argv[1];
+// }
+
+// void parse_args(t_gnl *gnl)
+// {
+
+// }
+
+// int main(int argc, char **argv, char **env)
+// {
+// 	t_gnl gnl;
+
+// 	init_gnl(&gnl, argc, argv, env);
+// 	parse_args(&gnl);
 // }
